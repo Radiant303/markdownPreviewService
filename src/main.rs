867728c6798cc2baab4af6c8b3d2ae47a9e63e57
ChenJunchao <junchao.chan@outlook.com>
2026-06-09 11:07:51 +0800

    尝试修复偏左问题

--- src/main.rs
+++ src/main.rs
@@ -1150,15 +1150,13 @@ fn char_visual_width(ch: char) -> f32 {
         0.0
     } else if ch.is_ascii() {
         match ch {
-            ' ' => 0.35,
-            'A'..='Z' => 0.66,
-            'a'..='z' | '0'..='9' => 0.58,
-            _ => 0.5,
+            ' ' => 0.40,
+            'A'..='Z' => 0.53,
+            'a'..='z' | '0'..='9' => 0.50,
+            _ => 0.45,
         }
-    } else if is_cjk_punctuation(ch) {
-        0.85
-    } else if is_cjk(ch) {
-        1.05
+    } else if is_cjk_punctuation(ch) || is_cjk(ch) {
+        1.00
     } else {
         0.9
     }
